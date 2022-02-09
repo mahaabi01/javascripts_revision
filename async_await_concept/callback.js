66c3846000afd5324d9b05b8de737dd501066b45
unknown <mahaabi01@gmail.com>
2022-02-09 20:48:59 +0545

    basic asynchronious concept

---
+++ async_await_concept/callback.js
@@ -0,0 +1,11 @@
+function add(a,b,callback){
+    setTimeout(()=> callback(a+b), 0);
+    // console.log('from function !')
+};
+function result(sum){
+    console.log("sum : " + sum);
+}
+console.log('Hello 1');
+add(1,2, result);
+console.log('Hello 2');
+
